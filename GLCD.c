ad8e7e18d96f6eed62b85d721bd6adccaf461f2b
mmatuschek <mmatuschek@sodiuswillert.com>
2023-08-22 15:49:59 +0200

    workitem 1380, #resolve

--- GLCD.c
+++ GLCD.c
@@ -22,7 +22,7 @@ a/*----------------------------------------------------------------------------
 #include <string.h>
 #include "GLCD.h"
 #include "font.h"
-Change
+Test für Kornelius
 /*********************** Hardware specific configuration **********************/
 
 /*------------------------- Speed dependant settings -------------------------*/
